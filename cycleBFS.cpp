4afa1eeaa7044e0c3448212a88cf18e26787a842
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-03-16 19:35:48 +0600

    error fix

--- cycleBFS.cpp
+++ cycleBFS.cpp
@@ -1,12 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    bool iscycle(int v , vector<int> adj[]){
-        for(int i = 1; i < v ; i++){
-            if(!vis[i]){
-                if(checkCycle(i , v, vis)) return true;
-            }
+bool checkCycle(int s , int v, vector<int> adj[] , vector <int> & visited){
+    queue<pair<int , int>> q;
+    visited[s] = true;
+    q.push({s,-1});
+
+    while (!q.empty())
+    {
+       int node  = q.front().first;
+       int par = q.front().second;
+       q.pop();
+
+       for(auto it : adj[node]){
+        if(!visited[it]){
+            visited[it] = true ; 
+            q.push({it, node});
+        }
+        else if(par != it){
+            return true;
+        }
+       }
+    }
+    return false;
+}
+
+bool iscycle(int v , vector<int> adj[]){
+    vector<int> vis(v, 0);
+    for(int i = 1; i < v ; i++){
+        if(!vis[i]){
+            if(checkCycle(i , v, adj, vis)) return true;
         }
     }
+    return false;
+}
+
+int main() {
+    int v, e;
+    cout << "Enter the number of vertices: ";
+    cin >> v;
+    cout << "Enter the number of edges: ";
+    cin >> e;
+    vector<int> adj[v];
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cout << "Enter edge #" << i + 1 << " as space-separated integers: ";
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    if (iscycle(v, adj)) {
+        cout << "The graph contains a cycle." << endl;
+    } else {
+        cout << "The graph does not contain a cycle." << endl;
+    }
+    return 0;
 }
